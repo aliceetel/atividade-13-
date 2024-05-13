9548d492b80cf74438aec86d710a4e6f69b08761
aliceetel <161315138+aliceetel@users.noreply.github.com>
2024-05-13 09:42:18 -0300

    Add files via upload

---
+++ exercicio js/atividade1.js
@@ -0,0 +1,22 @@
+function variavel() {
+    let n1 = parseInt(document.getElementById('numero1').value);
+    let n2 = parseInt(document.getElementById('numero2').value);
+    let d1 = 0;
+
+    let numero1 =prompt("informe o",n1,"primiero numero desejado:");
+    let numero2= prompt("informe o",n2,"segundo numero desejado:");
+    
+    if (n1>n2){
+        d1 = n1 - n2
+        console.log("A diferença é:",d1);
+    }else{
+        if(n2>n1){
+
+        }
+    }
+        //iner
+
+    }
+
+
+
